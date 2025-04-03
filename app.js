0944a3f2217afdb84c2d4a4db1427b79deebafd8
MARCIO MOREIRA <107818343+Moreira-Marcio@users.noreply.github.com>
2025-04-03 09:59:04 -0300

    adiciona apjs

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 import express from 'express';
 import {ler,inserir,lerUm,excluirUmAluno, atualizar} from './src/aluno.js';
+import cors from 'cors';
 
 
 
 const app = express();
-const porta = 3000;
+// o || (ou) é um operador logico onde mostra que a porta para conectar sera buscama caso nao ache ira para a porta 3000
+const porta = process.env.PORT || 3000;
 
 //habilitando para funcionar/dar suporte para o formato json
 app.use(express.json());
 
 //habilitando para dar suporte a dados inseridos a partir de imputs de formulario
 app.use(express.urlencoded({extended:true}));
+app.use(cors());
 
 
 app.get('/',(req,res)=>{
--- src/banco.js
+++ src/banco.js
@@ -2,18 +2,26 @@ import mysql from 'mysql2'; //importando um modulo
 
 //armazenar uma conexão em uma variavel
 
+//conexao local
 const conexao = mysql.createConnection({
-    hot: "locslhost",
-    user: "root",
-    password: "",
-    database: "escola-api"
+    hot: "db4free.net",
+    user: "bazao10",
+    password: "bazao10sp@",
+    database: "bazaoescola"
 });
+//const conexao = mysql.createConnection({
+  //  hot: "locslhost",
+    //user: "root",
+    //password: "",
+    //database: "escola-api"
+//});
 
 conexao.connect(erro => {
     if(erro){
         console.error(`erro ao conectar : ${erro.message}`);
     }else {
-        console.log(`banco de dados conectado com sucesso`)
+        console.log(`banco de dados conectado em:${conexao.config.host}.`)
+        //apresenta o host qual estamos conectados
     }
 });
 
